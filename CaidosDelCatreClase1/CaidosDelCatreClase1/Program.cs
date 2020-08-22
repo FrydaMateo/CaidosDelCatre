61cfecd0d8ab7de7d42fd6333f586157e5ec3ae9
FrydaMateo <frydasaezmateo@gmail.com>
2020-08-22 10:46:10 -0300

    DumbChanges

--- CaidosDelCatreClase1/CaidosDelCatreClase1/Program.cs
+++ CaidosDelCatreClase1/CaidosDelCatreClase1/Program.cs
@@ -12,11 +12,12 @@ namespace CaidosDelCatreClase1
         {
             double Monto = SolicitarMonto();
             double Interes = SolicitarInteres();
+            int Cuotas = SolicitarCuotas();
             string TipoAmortizacion = SolicitarAmortizacion();
 
             IAmortizable amortizacion = InstanciadorAmortizadora.ObtenerAmortizadora(TipoAmortizacion);
 
-            var dic = amortizacion.CalcularPagos(Monto, 12, Interes);
+            var dic = amortizacion.CalcularPagos(Monto, Cuotas, Interes);
 
             var exportador = new ExportadorHTML();
 
@@ -27,6 +28,22 @@ namespace CaidosDelCatreClase1
         {
             Console.WriteLine("Ingrese el tipo de amortización que desea utilizar");
             return Console.ReadLine();
+        }
+
+        private static int SolicitarCuotas()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Ingrese la cantidad de cuotas");
+                    return int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("La cantidad de cuotas debe ser un número");
+                }
+            }
         }
 
         private static double SolicitarInteres()
